96c46e7b0689b870997cf2d6c4c7b22a6562075b
Johannes Rosing <jrosing@kth.se>
2024-05-21 19:48:22 +0200

    test

--- helper_methods.py
+++ helper_methods.py
@@ -22,28 +22,41 @@ def get_train_val(filepath: str, val_size=0.2):
     return train_df, val_df
 
 
-def load_and_transform_image(image_path):
+def get_test(filepath: str):
+    data = []
+    with open(file=filepath) as f:
+        for row in f.readlines():
+            row = row.strip().split(' ')
+            row[1], row[2], row[3] = int(row[1]), int(row[2]), int(row[3])
+            data.append(row)
+    df = pd.DataFrame(data=data, columns=['image_id', 'class_id', 'species', 'breed'])
+    
+    # Replace 1 with 0 (for cats) and 2 with 1 (for dogs)
+    df['species'] = df['species'].replace({1: 0, 2: 1})
+    return df
+
+
+def load_and_transform_image(image_path, transformations, augment):
     """ Load an image and apply the transformations. """
     
     # Define the transformations (we use the same as what Resnet used for efficient transfer)
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    if augment:
+        # Add data augmentation on training data
+        transformations.append(transforms.RandomHorizontalFlip())
+        transformations.append(transforms.RandomRotation(10))
+    transform = transforms.Compose(transformations)
     image = Image.open(image_path).convert('RGB')  # Convert all images to RGB
     return transform(image)
 
 
 # Function to create a tensor dataset from DataFrame and transformations
-def create_dataset(df, base_path):
+def create_dataset(df, base_path, transformations, augment=False):
     images_tensors = []
     Y = []
     for row in df.values.tolist():
         image_id, species_id = row[0], row[2]
         image_path = f"{base_path}{image_id}.jpg"  # Adjust format as needed
-        image_tensor = load_and_transform_image(image_path)
+        image_tensor = load_and_transform_image(image_path, transformations, augment)
         images_tensors.append(image_tensor)
         Y.append(species_id)
     
@@ -78,3 +91,14 @@ def show_images(images, labels, n_images=None, figsize=(8, 8)):
 
     plt.tight_layout()
     plt.show()
+
+if __name__ == '__main__':
+    df_train, df_val = get_train_val(filepath='datasets/annotations/trainval.txt', val_size=0.2)
+    transformations = [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]
+    X_train, Y_train = create_dataset(df_train, base_path='datasets/images/', transformations=transformations, val=False)
+    X_val, Y_val = create_dataset(df_val, base_path='datasets/images/', transformations=transformations, val=True)
